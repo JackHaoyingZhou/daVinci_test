dcd70480cc97c0ed1adde56a4e09694023b96e1c
richardmohabirnyc <71954876+richardmohabirnyc@users.noreply.github.com>
2021-04-26 21:50:59 -0400

    Add files via upload

---
+++ sin_cos_tests.py
@@ -0,0 +1,108 @@
+
+from GaitAnaylsisToolkit.LearningTools.Runner import TPGMMRunner
+from GaitAnaylsisToolkit.LearningTools.Trainer import TPGMMTrainer
+from random import seed
+from random import gauss
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D
+import numpy.polynomial.polynomial as poly
+
+def coef(b, dt):
+
+    A = np.array([[1.0, 0, 0, 0],
+                  [0, 1.0, 0, 0],
+                  [1.0, dt, dt**2, dt**3],
+                  [0, 1.0, 2*dt, 3*dt**2]])
+
+
+    return np.linalg.pinv(A).dot(b)
+
+
+# seed random number generator
+seed(1)
+count = 200
+# b = np.array([ [-0.267], [0.], [ -0.7 ], [0.0] ])
+# x = coef(b, 10)
+# fit = poly.Polynomial(x.flatten())
+t = np.linspace(0,10,count)
+x_prime = np.cos(t)
+
+x_data = []
+for i in range(5):
+    x = x_prime + gauss(-.1, .1)
+    x_data.append(x)
+
+# b = np.array([ [0.221], [0.0], [0.5], [0.0] ])
+# x = coef(b, 10)
+# fit = poly.Polynomial(x.flatten())
+t = np.linspace(0, 10, count)
+y_prime = np.sin(t)
+#Y Coordinate
+y_data = []
+for i in range(5):
+    y = y_prime + gauss(-.1, .1)
+    y_data.append(y)
+
+
+# b = np.array([ [-0.147], [0.0], [-0.2 ], [0.0] ])
+# x = coef(b, 10)
+# fit = poly.Polynomial(x.flatten())
+t = np.linspace(0,10,count)
+z_prime = t
+#Z Coordinate
+z_data = []
+for i in range(5):
+    z = 3*(z_prime + gauss(-.1, .1))
+    z_data.append(z)
+
+
+trainer = TPGMMTrainer.TPGMMTrainer(demo=[x_data, y_data, z_data],
+                                    file_name="simpletest",
+                                    n_rf=3,
+                                    dt=0.01,
+                                    reg=[1e-3],
+                                    poly_degree=[3,3,3])
+trainer.train()
+runner = TPGMMRunner.TPGMMRunner("simpletest")
+
+
+path = runner.run()
+x_coordinates = path[:,0]
+y_coordinates = path[:,1]
+z_coordinates = path[:,2]
+
+x_data_arr = np.array(x_data)
+y_data_arr = np.array(y_data)
+z_data_arr = np.array(z_data)
+print(x_data_arr[0, :])
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter3D(x_data_arr[0, :], y_data_arr[0, :], z_data_arr[0, :], c='yellow')
+ax.scatter3D(x_data_arr[1, :], y_data_arr[1, :], z_data_arr[1, :], c='blue')
+ax.scatter3D(x_data_arr[2, :], y_data_arr[2, :], z_data_arr[2, :], c='green')
+ax.scatter3D(x_data_arr[3, :], y_data_arr[3, :], z_data_arr[3, :], c='purple')
+ax.scatter3D(x_data_arr[4, :], y_data_arr[4, :], z_data_arr[4, :], c='orange')
+ax.scatter3D(x_coordinates, y_coordinates, z_coordinates, c='red')
+ax.legend(['Given Trajectory #1', 'Given Trajectory #2', 'Given Trajectory #3', 'Given Trajectory #4', 'Given Trajectory #5',  'TPGMM Genereated Trajectory'], loc="left", ncol=1)
+ax.set_title("Given Trajectories vs. TPGMM Generated Trajectory")
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+plt.tight_layout()
+plt.show()
+
+# fig, axs = plt.subplots(3)
+
+
+# for p in knee:
+#     axs[1].plot(p)
+#     axs[1].plot(path[:, 1], linewidth=4, color='black')
+
+# for p in ankle:
+#     axs[2].plot(p)
+#     axs[2].plot(path[:, 2], linewidth=4, color='black')
+
+# plt.show()
